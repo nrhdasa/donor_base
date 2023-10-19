46e472b97f919b1721db495e58053b08b4216193
HKMA <harekrishnamandir.ahmd@gmail.com>
2023-10-19 10:40:44 +0000

    misc

--- donor_base/donor_base/doctype/db_online_donor_profile/db_online_donor_profile.py
+++ donor_base/donor_base/doctype/db_online_donor_profile/db_online_donor_profile.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2023, Narahari Dasa and contributors
 # For license information, please see license.txt
 
+from frappe.model.document import Document
 
-
+class DBOnlineDonorProfile(Document):
+	pass
--- donor_base/donor_base/doctype/db_online_donor_sms/db_online_donor_sms.py
+++ donor_base/donor_base/doctype/db_online_donor_sms/db_online_donor_sms.py
@@ -1,3 +1,7 @@
 # Copyright (c) 2023, Narahari Dasa and contributors
 # For license information, please see license.txt
 
+from frappe.model.document import Document
+
+class DBOnlineDonorSMS(Document):
+	pass
